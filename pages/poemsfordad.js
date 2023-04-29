66ddf8a053b238f93e8e8e42e4fc3c83006cc36e
Steve M <stevechez@gmail.com>
2023-04-29 09:49:07 -0700

    fix poem page

--- pages/poemsfordad.js
+++ pages/poemsfordad.js
@@ -2,25 +2,26 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="container md:w-11/12 mx-auto">
+    <main className="container md:w-full first-letter:lg:w-11/12 mx-auto">
       <h1 className="abrilFF text-6xl font-bold text-center mt-14 mb-4">
         Thinking of my dad
       </h1>
 
-      <p className="petrona w-full my-12 text-3xl text-center md:my-12 px-4 md:px-8 text-sky-900">
-        To my dear father, strong and true,
-        <br />A man whose love shines through and through, <br />
-        You&apos;ve always been my guiding light, <br />
-        Through every trial and every fight.
-      </p>
-
-      <div className="flex flex-col justify-center md:w-3/4 mx-auto">
-        <p className="sourcesans text-xl m-4">
-          From one of dad&apos;s favorite tapes of the 80&apos;s. Christopher
-          Cross.
+      <div className="flex flex-col justify-center md:w-full mx-auto">
+        <p className="petrona text-3xl text-center my-12 px-0 md:px-20 text-sky-900">
+          To my dear father, strong and true,
+          <br />A man whose love shines through and through, <br />
+          You&apos;ve always been my guiding light, <br />
+          Through every trial and every fight.
         </p>
 
-        {/* <p className="petrona text-3xl text-center mb-8 px-4 md:px-20 text-sky-900">
+        <div className="flex flex-col justify-center md:w-full mx-auto">
+          <p className="sourcesans text-xl m-4 text-left md:text-center">
+            From one of dad&apos;s favorite tapes of the 80&apos;s. Christopher
+            Cross.
+          </p>
+
+          {/* <p className="petrona text-3xl text-center mb-8 px-4 md:px-20 text-sky-900">
         I will remember dad vividly from the earliest days of my childhood. He
         the patriarch, a father who worked hard, took care of the family, kept
         roof over our head, food on the table, made it possible to live in a
@@ -30,16 +31,17 @@ export default function Home() {
         This song is from the one tape I think dad ever owned, Christopher
         Cross.
       </p> */}
-        <div className="video-responsive">
-          <iframe
-            width="853"
-            height="480"
-            src={`https://www.youtube.com/embed/KR71fkBhBSc`}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            title="Embedded youtube"
-            className="px-8"
-          />
+          <div className="video-responsive">
+            <iframe
+              width="853"
+              height="480"
+              src={`https://www.youtube.com/embed/KR71fkBhBSc`}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              title="Embedded youtube"
+              className="md:w-5/6 md:mx-auto"
+            />
+          </div>
         </div>
       </div>
 
@@ -48,7 +50,7 @@ export default function Home() {
         fallen, lent a shoulder when I needed someone, and gave encouragement.
         This, as a child was one of the first songs I heard my dad play:
       </p> */}
-      <div className="flex flex-col justify-center md:w-3/4 mx-auto">
+      <div className="flex flex-col justify-center md:w-full mx-auto">
         <p className="petrona text-3xl text-center my-12 px-0 md:px-20 text-sky-900">
           Your wisdom and your steady hand, <br />
           Have helped me grow and understand, <br />
@@ -56,7 +58,7 @@ export default function Home() {
           And how to navigate life&apos;s endless night.
         </p>
 
-        <p className="sourcesans text-xl m-4">
+        <p className="sourcesans text-xl m-4 text-left md:text-center">
           One of his favorite singers of the 70&apos; as I recall as a young
           kid.
         </p>
@@ -70,18 +72,18 @@ export default function Home() {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
             title="Embedded youtube"
-            className="px-8"
+            className="md:w-5/6 md:mx-auto"
           />
         </div>
       </div>
-      <div className="flex flex-col justify-center md:w-3/4 mx-auto">
+      <div className="flex flex-col justify-center md:w-full mx-auto">
         <p className="petrona text-3xl text-center my-12 px-4 md:px-20 text-sky-900">
           So on this day, I honor you, <br />
           And all the things that you do, <br />
           My father, my friend, my rock, <br />I thank you for each precious
           clock.
         </p>
-        <p className="sourcesans text-xl m-4">
+        <p className="sourcesans text-xl m-4 text-left md:text-center">
           A song that we listened together on multiple solo trips together.
         </p>
         <div className="video-responsive">
@@ -93,11 +95,11 @@ export default function Home() {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
             title="Embedded youtube"
-            className="px-8"
+            className="md:w-5/6 md:mx-auto"
           />
         </div>
       </div>
-      <div className="flex flex-col justify-center md:w-3/4 mx-auto">
+      <div className="flex flex-col justify-center md:w-full mx-auto">
         <p className="petrona text-3xl text-center my-12 px-4 md:px-20 text-sky-900">
           A mentor, friend, and hero too, <br />
           Your heart and soul a shining light, <br />I feel so blessed to be
@@ -113,11 +115,11 @@ export default function Home() {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
             title="Embedded youtube"
-            className="px-8"
+            className="md:w-5/6 md:mx-auto"
           />
         </div>
       </div>
-      <div className="flex flex-col justify-center md:w-3/4 mx-auto">
+      <div className="flex flex-col justify-center md:w-full mx-auto">
         <p className="petrona text-3xl text-center my-12 px-4 md:px-20 text-sky-900">
           Your sacrifices I can never repay, <br />
           Your guidance I&apos;ll forever cherish, <br />
@@ -133,7 +135,7 @@ export default function Home() {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
             title="Embedded youtube"
-            className="px-8"
+            className="md:w-5/6 md:mx-auto"
           />
         </div>
       </div>
